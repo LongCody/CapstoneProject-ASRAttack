2253272da45eda1d4658a618677204c24efe0303
Cody Long <codylong@nevada.unr.edu>
2023-12-05 09:55:18 -0800

    Correct data collator

--- Scripts/peftwav2vec2.py
+++ Scripts/peftwav2vec2.py
@@ -1,29 +1,33 @@
-import os
 import gc
-import torch
+import os
 import evaluate
+import torch
 import numpy as np
-from torch.utils.data import DataLoader
 from tqdm import tqdm
-from datasets import load_dataset, Audio,  DatasetDict
 from dataclasses import dataclass, field
-from peft import LoraConfig, PeftModel, LoraModel, get_peft_model
-from typing import Any, Dict, List, Optional, Union
+from datasets import load_dataset, Audio, DatasetDict
+from huggingface_hub import notebook_login
+from peft import LoraConfig, PeftModel, LoraModel, LoraConfig, get_peft_model
+from torch.utils.data import DataLoader
+from typing import Any, Dict, List, Union
+from transformers import AutoFeatureExtractor, AutoTokenizer, AutoProcessor, AutoModelForCTC, TrainingArguments, Trainer, TrainerState, TrainerControl, TrainerCallback
 from transformers.trainer_utils import PREFIX_CHECKPOINT_DIR
-from transformers import AutoFeatureExtractor, AutoTokenizer, AutoProcessor, AutoModelForSpeechSeq2Seq, Seq2SeqTrainingArguments, Seq2SeqTrainer, TrainerCallback, Seq2SeqTrainingArguments, TrainerState, TrainerControl
 
 os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 model_name_or_path = "facebook/wav2vec2-base"
+language = "English"
 task = "transcribe"
 dataset_name = "librispeech_asr"
 
+notebook_login()
+
 librispeech = DatasetDict()
 librispeech = load_dataset(dataset_name, split = "train.clean.100")
-librispeech = librispeech.train_test_split(test_size=0.2)
+librispeech = librispeech.train_test_split(test_size = 0.2)
 
 feature_extractor = AutoFeatureExtractor.from_pretrained(model_name_or_path)
-tokenizer = AutoTokenizer.from_pretrained(model_name_or_path, task=task)
-processor = AutoProcessor.from_pretrained(model_name_or_path, task=task)
+tokenizer = AutoTokenizer.from_pretrained(model_name_or_path, language=language, task=task)
+processor = AutoProcessor.from_pretrained(model_name_or_path, language=language, task=task)
 
 librispeech = librispeech.remove_columns(["file", "speaker_id", "chapter_id", "id"])
 
@@ -33,59 +37,100 @@ def prepare_dataset(batch):
     batch["labels"] = tokenizer(batch["text"]).input_ids
     return batch
 
-librispeech = librispeech.map(prepare_dataset, remove_columns=librispeech.column_names["train"], num_proc=2)
+encoded_librispeech = librispeech.map(prepare_dataset, remove_columns=librispeech.column_names["train"], num_proc=2)
 
 @dataclass
-class DataCollatorSpeechSeq2SeqWithPadding:
-    processor: Any
+class DataCollatorCTCWithPadding:
+    processor: AutoProcessor
+    padding: Union[bool, str] = "longest"
 
     def __call__(self, features: List[Dict[str, Union[List[int], torch.Tensor]]]) -> Dict[str, torch.Tensor]:
-        input_features = [{"input_features": feature["input_features"]} for feature in features]
-        batch = self.processor.feature_extractor.pad(input_features, return_tensors="pt")
-
+        # split inputs and labels since they have to be of different lengths and need
+        # different padding methods
+        input_features = [{"input_values": feature["input_values"][0]} for feature in features]
         label_features = [{"input_ids": feature["labels"]} for feature in features]
-        labels_batch = self.processor.tokenizer.pad(label_features, return_tensors="pt")
 
+        batch = self.processor.pad(input_features, padding=self.padding, return_tensors="pt")
+
+        labels_batch = self.processor.pad(labels=label_features, padding=self.padding, return_tensors="pt")
+
+        # replace padding with -100 to ignore loss correctly
         labels = labels_batch["input_ids"].masked_fill(labels_batch.attention_mask.ne(1), -100)
-
-        if (labels[:, 0] == self.processor.tokenizer.bos_token_id).all().cpu().item():
-            labels = labels[:, 1:]
 
         batch["labels"] = labels
 
         return batch
+    
+data_collator = DataCollatorCTCWithPadding(processor=processor, padding="longest")
 
+model = AutoModelForCTC.from_pretrained(
+    "facebook/wav2vec2-base",
+    ctc_loss_reduction="mean",
+    pad_token_id=processor.tokenizer.pad_token_id,
+)
 
-data_collator = DataCollatorSpeechSeq2SeqWithPadding(processor=processor)
+config = LoraConfig(r=8, lora_alpha=32, target_modules=["k_proj", "q_proj"], lora_dropout=0.1, bias="True")
 
-model = AutoModelForSpeechSeq2Seq.from_pretrained(model_name_or_path, load_in_8bit=True, device_map="auto")
 model.config.forced_decoder_ids = None
 model.config.suppress_tokens = []
 
-peft_config = LoraConfig(inference_mode=False, r=8, lora_alpha=32, lora_dropout=0.1, target_modules=["k_proj", "q_proj"])
-model = get_peft_model(model, peft_config)
+def print_trainable_parameters(model):
+    trainable_params = 0
+    all_param = 0
+    for _, param in model.named_parameters():
+        all_param += param.numel()
+        if param.requires_grad:
+            trainable_params += param.numel()
+    print(
+        f"trainable params: {trainable_params} || all params: {all_param} || trainable%: {100 * trainable_params / all_param:.2f}"
+    )
+
+wer = evaluate.load("wer")
+
+def compute_metrics(pred):
+
+    pred_logits = pred.predictions
+
+    pred_ids = np.argmax(pred_logits, axis=-1)
+
+    pred.label_ids[pred.label_ids == -100] = processor.tokenizer.pad_token_id
+
+    pred_str = processor.batch_decode(pred_ids)
+
+    label_str = processor.batch_decode(pred.label_ids, group_tokens=False)
+
+    wer = wer.compute(predictions=pred_str, references=label_str)
+
+    return {"wer": wer}
+
+model = get_peft_model(model, config)
 model.print_trainable_parameters()
 
-training_args = Seq2SeqTrainingArguments(
-    output_dir="tni-lab/peft-facebook-wav2vec2-base-asr",
+training_args = TrainingArguments(
+    output_dir="my_awesome_asr_mind_model",
     per_device_train_batch_size=8,
-    gradient_accumulation_steps=1,
-    learning_rate=1e-3,
-    warmup_steps=50,
-    num_train_epochs=3,
-    evaluation_strategy="epoch",
+    gradient_accumulation_steps=2,
+    learning_rate=1e-5,
+    warmup_steps=500,
+    max_steps=2000,
+    gradient_checkpointing=True,
     fp16=True,
+    group_by_length=True,
+    evaluation_strategy="steps",
     per_device_eval_batch_size=8,
-    generation_max_length=128,
+    save_steps=1000,
+    eval_steps=1000,
     logging_steps=25,
-    remove_unused_columns=False,
-    label_names=["labels"],
+    load_best_model_at_end=True,
+    metric_for_best_model="wer",
+    greater_is_better=False,
+    push_to_hub=True,
 )
 
 class SavePeftModelCallback(TrainerCallback):
     def on_save(
         self,
-        args: training_args,
+        args: TrainingArguments,
         state: TrainerState,
         control: TrainerControl,
         **kwargs,
@@ -99,49 +144,17 @@ class SavePeftModelCallback(TrainerCallback):
         if os.path.exists(pytorch_model_path):
             os.remove(pytorch_model_path)
         return control
-    
-trainer = Seq2SeqTrainer(
-    args=training_args,
+
+trainer = Trainer(
     model=model,
-    train_dataset=librispeech["train"],
-    eval_dataset=librispeech["test"],
-    data_collator=data_collator,
+    args=training_args,
+    train_dataset=encoded_librispeech["train"],
+    eval_dataset=encoded_librispeech["test"],
     tokenizer=processor.feature_extractor,
-    callbacks=[SavePeftModelCallback],
+    data_collator=data_collator,
+    compute_metrics=compute_metrics,
+    callbacks = [SavePeftModelCallback]
 )
-model.config.use_cache = False
+
 trainer.train()
-
-metric = evaluate.load("wer")
-
-from torch.utils.data import DataLoader
-from tqdm import tqdm
-import numpy as np
-import gc
-
-eval_dataloader = DataLoader(librispeech["test"], batch_size=8, collate_fn=data_collator)
-
-model.eval()
-for step, batch in enumerate(tqdm(eval_dataloader)):
-    with torch.no_grad():
-        generated_tokens = (
-            model.generate(
-                input_features=batch["input_features"].to("cuda"),
-                decoder_input_ids=batch["labels"][:, :4].to("cuda"),
-                max_new_tokens=255,
-            )
-            .cpu()
-            .numpy()
-        )
-        labels = batch["labels"].cpu().numpy()
-        labels = np.where(labels != -100, labels, tokenizer.pad_token_id)
-        decoded_preds = tokenizer.batch_decode(generated_tokens, skip_special_tokens=True)
-        decoded_labels = tokenizer.batch_decode(labels, skip_special_tokens=True)
-        metric.add_batch(
-            predictions=decoded_preds,
-            references=decoded_labels,
-        )
-    del generated_tokens, labels, batch
-    gc.collect()
-wer = 100 * metric.compute()
-print(f"{wer=}")
+model.push_to_hub("cjccl/labeledwav2vec2-base")
