40fa133b93f19a798ffd248e0702b7e08e9bebb0
Jacob <66694749+Jacob7532@users.noreply.github.com>
2023-12-13 18:10:36 -0800

    Update NavBar.js

--- GUI/asr-attack/src/NavBar.js
+++ GUI/asr-attack/src/NavBar.js
@@ -1,6 +1,7 @@
 import { Link } from 'react-router-dom';
 
 export default function NavBar() {
+    //Navigation Bar components
     return (
         <nav className="nav">
             <Link to="/" className="site-title">ASR Attack</Link>
