b2116b4b1d0b25d6407a903b333c0a53dca4815f
Strazi-Zachary <zstrazi@nevada.unr.edu>
2024-02-25 15:39:56 -0800

    Unit Test Script

---
+++ Scripts/Unittests.py
@@ -0,0 +1,10 @@
+from rpi import rpi
+from Whisper_Speech_to_Text import Whisper_Speech_to_Text
+from Whisper_WER_Test import Whisper_WER_TEST
+import unittest
+import unittest.mock
+
+class Test_ASRProject(unittest.TestCase):
+    
+    def test_rpi():
+        pass
