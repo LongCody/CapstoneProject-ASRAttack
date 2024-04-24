ca04c6badfbc4a3551249847854b5b00d05f3fa4
Jacob <66694749+Jacob7532@users.noreply.github.com>
2024-04-24 09:42:50 -0700

    Update Try.js
    fixed formating

--- asr-attack/src/pages/Try.js
+++ asr-attack/src/pages/Try.js
@@ -10,12 +10,15 @@ export default function Try() {
         <Box sx={{ maxWidth: 'auto', margin: 'auto', padding: 4 }}>
              <Paper sx={{ padding: 2, backgroundColor: '#e0e0e0', boxShadow: '0px 4px 10px rgba(0, 0, 0, 0.3)'}}>
                 <Paper sx={{ padding: 2, backgroundColor: '#f5f5f5', boxShadow: '0px 4px 10px rgba(0, 0, 0, 0.3)'}}>
-                    <Typography variant="body1" paragraph>
+                    <Typography variant="h2" gutterBottom sx={{ textAlign: 'center' }}>Try It!</Typography>
+                </Paper>
+                <br />
+                <Paper sx={{ padding: 2, backgroundColor: '#f5f5f5', boxShadow: '0px 4px 10px rgba(0, 0, 0, 0.3)'}}>
+                    <Typography variant="h6" gutterBottom sx={{ textAlign: 'center' }}>
                         Press the button to test our script that listens for a wake word for your home assistant such as "hey google", then plays an adversarial example.
                     </Typography>
                 </Paper>
-            </Paper>
-            <Paper sx={{ padding: 2, backgroundColor: '#e0e0e0', boxShadow: '0px 4px 10px rgba(0, 0, 0, 0.3)'}}>
+                <br />
                 <Paper sx={{ padding: 2, backgroundColor: '#f5f5f5', boxShadow: '0px 4px 10px rgba(0, 0, 0, 0.3)'}}>
                     <Button variant="contained" onClick={rpiScript} sx={{ textAlign: 'center', display: 'block', margin: 'auto' }}>Try it here!</Button>
                 </Paper>
