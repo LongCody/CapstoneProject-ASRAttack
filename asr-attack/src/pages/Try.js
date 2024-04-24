9f6af987546f72c0b8652c6f28c1cf6441b5c738
Kristian Konstantinov <70545958+KKonstantinov-UNR@users.noreply.github.com>
2024-04-24 01:22:01 -0700

    Update Try.js

--- asr-attack/src/pages/Try.js
+++ asr-attack/src/pages/Try.js
@@ -1,16 +1,23 @@
 import { Box, Typography, Paper, Button } from '@mui/material';
 import { Link } from 'react-router-dom';
 
-function loadRPI(){
-    window.indexBridge.loadRPI();
+function rpiScript(){
+    window.open("PyCode.html", "_blank");
 }
 
 export default function Try() {
     return (
         <Box sx={{ maxWidth: 'auto', margin: 'auto', padding: 4 }}>
+             <Paper sx={{ padding: 2, backgroundColor: '#e0e0e0', boxShadow: '0px 4px 10px rgba(0, 0, 0, 0.3)'}}>
+                <Paper sx={{ padding: 2, backgroundColor: '#f5f5f5', boxShadow: '0px 4px 10px rgba(0, 0, 0, 0.3)'}}>
+                    <Typography variant="body1" paragraph>
+                        Press the button to test our script that listens for a wake word for your home assistant such as "hey google", then plays an adversarial example.
+                    </Typography>
+                </Paper>
+            </Paper>
             <Paper sx={{ padding: 2, backgroundColor: '#e0e0e0', boxShadow: '0px 4px 10px rgba(0, 0, 0, 0.3)'}}>
                 <Paper sx={{ padding: 2, backgroundColor: '#f5f5f5', boxShadow: '0px 4px 10px rgba(0, 0, 0, 0.3)'}}>
-                    <Button variant="contained" id="RPI_Script" onclick="loadRpi();" sx={{ textAlign: 'center', display: 'block', margin: 'auto' }}>Try it here!</Button>
+                    <Button variant="contained" onClick={rpiScript} sx={{ textAlign: 'center', display: 'block', margin: 'auto' }}>Try it here!</Button>
                 </Paper>
             </Paper>
         </Box>
