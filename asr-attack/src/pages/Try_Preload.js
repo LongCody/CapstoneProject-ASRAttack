e9ef155d2806543637b4f34ee85fff7907a62145
Kristian Konstantinov <70545958+KKonstantinov-UNR@users.noreply.github.com>
2024-04-24 01:19:49 -0700

    Delete asr-attack/src/pages/Try_Preload.js

--- asr-attack/src/pages/Try_Preload.js
+++
@@ -1,9 +0,0 @@
-const { contextBridge, ipcMain, ipcRenderer} = require('electron')
-
-let indexBridge = {   
-    loadRPI: async () => {
-        await ipcRenderer.invoke("loadRpi");
-    },
-}
-
-contextBridge.exposeInMainWorld("indexBridge", indexBridge);
