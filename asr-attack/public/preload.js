a2531b1a7dbb40ea54c773b0d7a86f157bf2a6cc
Kristian Konstantinov <70545958+KKonstantinov-UNR@users.noreply.github.com>
2024-04-24 01:20:10 -0700

    Delete asr-attack/public/preload.js

--- asr-attack/public/preload.js
+++
@@ -1,8 +0,0 @@
-const { contextBridge, ipcMain, ipcRenderer } = require('electron');
-
-const indexBridge = require('./pages/Try_Preload')
-
-if (location.href.endsWith('Try.js')) {
-  Bridge = indexBridge
-}
-contextBridge.exposeInMainWorld('Bridge', Bridge)
