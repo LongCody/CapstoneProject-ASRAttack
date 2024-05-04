cc6b537893be10f6684eda93bb5dc813b9699ad7
Strazi-Zachary <zstrazi@nevada.unr.edu>
2024-05-04 15:46:10 -0700

    Comment Amazon

--- Scripts/amazontranscribe.py
+++ Scripts/amazontranscribe.py
@@ -2,10 +2,10 @@ import boto3
 import urllib
 import json
 
-transcribe_client = boto3.client('transcribe', region_name = 'us-west-1', aws_access_key_id=<'access_key'>,
-         aws_secret_access_key= <'secret_access_key'>
+transcribe_client = boto3.client('transcribe', region_name = 'us-west-1', aws_access_key_id='access_key', #AWS account access info
+         aws_secret_access_key= 'secret_access_key')
 
-def transcribefile(jobname, file_uri, transcribe_client):
+def transcribefile(jobname, file_uri, transcribe_client): #Transcribes an audio file by job name in desired language and by audiofile type
     transcribe_client.start_transcription_job(
         TranscriptionJobName = job_name,
         Media = {'MediaFileUri': file_uri},
@@ -16,9 +16,9 @@ def transcribefile(jobname, file_uri, transcribe_client):
     max_tries = 15
     while max_tries > 0:
         max_tries = -1
-        job = transcribe_client.get_transcription_job(TranscriptionJobName = job_name)
+        job = transcribe_client.get_transcription_job(TranscriptionJobName = job_name) #Gets Transcription job status
         job_status = job['TranscriptionJob']['TranscriptionJobStatus']
-        if job_status in ['COMPLETED', 'FAILED']:
+        if job_status in ['COMPLETED', 'FAILED']: #Checks if the job is completed or failed, if completed gets and prints transcription
                 print(f"Job {job_name} is {job_status}.")
                 if job_status == 'COMPLETED':
                     response = urllib.request.urlopen(job['TranscriptionJob']['Transcript']['TranscriptFileUri'])
@@ -29,8 +29,8 @@ def transcribefile(jobname, file_uri, transcribe_client):
                 break
 
 def main():
-     file_url = "s3://team04bucketunr/willthisswork.wav"
-     transcribe_file('TESTJOBTEAM04difft', file_url, transcribe_client)
+     file_url = "s3://team04bucketunr/willthisswork.wav" #Bucket URL for audio file
+     transcribefile('TESTJOBTEAM04difft', file_url, transcribe_client) #Job Detail
 
-if __name == '__main':
-    main(
+if __name__ == '__main':
+    main()
